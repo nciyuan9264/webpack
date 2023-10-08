30dedf25c201453c453cc912531e3925cab5b8b7
ZhenyuWang <zhenyu.wang1@eeoa.com>
2023-10-08 18:56:48 +0800

    feat: webpack测试

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+    return (
+        <canvas width='500px' height='500px' style={{background: 'blue', opacity: 0.3}}></canvas>
+    )
+}
+
+export default App
